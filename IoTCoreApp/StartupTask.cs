b5c2df9a9925487ccc50fca2673c96673ca7e355
Ville Rantala <ville.rantala@microsoft.com>
2015-11-15 16:10:08 +0200

    Generated project template.

---
+++ IoTCoreApp/StartupTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace IoTCoreApp
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to start one or more asynchronous methods 
+            //
+        }
+    }
+}
